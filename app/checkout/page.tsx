3a6b098e7ddd1a1ae143f135da39644005ebe2e7
Kamrul Hasan <kamrul.h456@gmail.com>
2025-05-25 23:44:55 +0600

    Initial Project setup

---
+++ app/checkout/page.tsx
@@ -0,0 +1,9 @@
+export default function CheckoutPage() {
+    return (
+        <div>
+        <h1>Checkout Page</h1>
+        <p>Thank you for your order!</p>
+        <p>Your order will be processed shortly.</p>
+        </div>
+    );
+}
---
+++ app/products/page.tsx
@@ -0,0 +1,9 @@
+export default function ProductsPage() {
+    return (
+        <div>
+            <h1>Products Page</h1>
+            <p>Here you can find a variety of products.</p>
+            <p>Browse through our collection and add items to your cart.</p>
+        </div>
+    );
+}
